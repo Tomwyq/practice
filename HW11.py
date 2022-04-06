d9865f4b65a30534b4ee62a6b4ab6cf450437173
Tomwyq <103146158+Tomwyq@users.noreply.github.com>
2022-04-06 19:36:25 -0400

    Add files via upload

---
+++ HW11.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 19:33:23 2022
+
+@author: User
+"""
+
+def WhoAmI():
+    return('yw3696')
+WhoAmI()
+
+# getBondPrice
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    bondPrice = 2
+    return(bondPrice)
+
+# Test values
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
+ppy = 1
+ppy = 2
+
